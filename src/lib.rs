a7715053fe8a457c47556c63ead6e28e91621014
GameGuy <96356176+GameGuyThrowaway@users.noreply.github.com>
2025-09-28 06:36:28 -0400

    Removed USB device detection. Made redundant by previous commit.

--- src/lib.rs
+++ src/lib.rs
@@ -42,14 +42,6 @@ const COMPORT_CLASS_GUID: GUID = GUID {
     Data4: [0x9C, 0xE4, 0x08, 0x00, 0x3E, 0x30, 0x1F, 0x73],
 };
 
-/// GUID_DEVINTERFACE_USB_DEVICE
-const USB_CLASS_GUID: GUID = GUID {
-    Data1: 0xA5DCBF10,
-    Data2: 0x6530,
-    Data3: 0x11D2,
-    Data4: [0x90, 0x1F, 0x00, 0xC0, 0x4F, 0xB9, 0x51, 0xED],
-};
-
 ///
 /// `list_com_ports` iterates over all available devices in the SerialPorts
 /// class, returning a list of their data represented as SerialPortInfo.
@@ -70,53 +62,6 @@ pub fn list_com_ports() -> Result<Vec<SerialPortInfo>, NTSTATUS> {
     let interfaces_status = unsafe {
         IoGetDeviceInterfaces(
             &COMPORT_CLASS_GUID,
-            device,
-            0,
-            &mut symbolic_link_list as *mut PZZWSTR,
-        )
-    };
-
-    if !nt_success(interfaces_status) {
-        return Err(interfaces_status);
-    }
-
-    let symbolic_list = SerialPortInfo::from_wstr_array(symbolic_link_list);
-
-    // SAFETY: This is safe because:
-    //         1. `symbolic_link_list` is not freed anywhere else.
-    unsafe {
-        ExFreePool(symbolic_link_list as *mut _);
-    }
-
-    Ok(symbolic_list)
-}
-
-///
-/// `list_usb_ports` iterates over all available devices in the USB Devices
-/// class, returning a list of their data represented as SerialPortInfo.
-///
-/// Be careful using this function. While `list_com_ports` returns device info
-/// that should always be safe to open, most USB device are not com devices,
-/// and so may have undefined behavior to com requests. Be sure to check the
-/// device's VID/PID, or find it in the Registry to verify that it is a virtual
-/// com device.
-///
-/// # Return value:
-///
-/// * `Ok(Vec<SerialPortInfo>)` - The list of all ports' info, if successful.
-/// * `Err(NTSTATUS)` - Otherwise, with the status of the failed
-///   IoGetDeviceInterfaces call.
-///
-pub fn list_usb_ports() -> Result<Vec<SerialPortInfo>, NTSTATUS> {
-    let device: PDEVICE_OBJECT = null_mut();
-    let mut symbolic_link_list: PZZWSTR = null_mut();
-
-    // SAFETY: This is safe because:
-    //         1. `device` is allowed to be null.
-    //         2. `symbolic_link_list` is allowed to be null.
-    let interfaces_status = unsafe {
-        IoGetDeviceInterfaces(
-            &USB_CLASS_GUID,
             device,
             0,
             &mut symbolic_link_list as *mut PZZWSTR,
